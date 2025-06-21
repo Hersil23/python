e302d95709be78f3c453b34b92e7a886a523d9c2
Herasi Silva <herasidesweb@gmail.com>
2025-06-20 22:18:08 -0400

    Python

---
+++ Curso-de-Python/Python-Basico/variables.py
@@ -0,0 +1,45 @@
+#Soy un comentario de una linea
+"""Soy un comentario de varias lineas
+Este es un ejemplo de variables en Python.
+Variables son espacios en memoria que almacenan datos.
+"""
+# Variables
+
+name = "Herasi"  
+
+# tipos de datos
+# enteros 
+age = 30  # entero
+
+# flotantes
+height = 1.75  # flotante
+
+# enteros negativos
+negative_number = -10  # entero negativo    
+
+# texto
+text = "Hola, Python!"  # cadena de texto (string)
+
+# booleanos
+is_active = True  # booleano (True o False)
+
+#listas
+fruits = ["apple", "banana", "cherry"]  # lista de frutas
+
+numbers = [1, 2, 3, 4, 5]  # lista de números
+
+Students = ["Alice", "Bob", "Charlie"]  # lista de estudiantes
+
+componets = ["CPU", "RAM", "SSD", ["SSD", "HDD"]]  # lista de componentes de una computadora
+
+#tuplas (colecciones inmutables)
+
+coordinates = (10.0, 20.0)  # tupla de coordenadas (x, y)
+
+#diccionarios (colecciones de pares clave-valor)
+person = {
+  "name": "Alice",
+  "age": 30,
+  "city": "New York"
+}  # diccionario con información de una persona
+
